6a7cb96788a22623211571de541fb04a37ebc15c
lleck <lleck@192.168.18.24>
2025-09-30 21:05:13 -0500

    Carga inicial

---
+++ src/proyectofinaltorres/ProyectoFinalTorres.java
@@ -0,0 +1,27 @@
+package proyectofinalTorres;
+import java.util.Scanner;
+public class ProyectoFinalTorres {
+    public static void main(String[] args) {
+               Scanner sc = new Scanner(System.in);
+        double compra1, compra2, compra3, totalcompras, promediocompras;
+        // ingreso de datos
+        System.out.print("Ingrese el nombre del cliente: ");
+        String nombre = sc.nextLine();
+        System.out.print("Ingrese el valor de la compra 1: ");
+        compra1 = sc.nextDouble();
+        System.out.print("Ingrese el valor de la compra 2: ");
+        compra2 = sc.nextDouble();
+        System.out.print("Ingrese el valor de la compra 3: ");
+        compra3 = sc.nextDouble();
+
+        // proceso de datos
+        totalcompras = compra1 + compra2 + compra3;
+        promediocompras = totalcompras / 3;
+
+        // Salida de datos
+        System.out.println("Cliente: " + nombre);
+        System.out.println("Total de compras   : " + totalcompras);
+        System.out.println("Promedio de compras: " + promediocompras);
+    }
+    
+}
